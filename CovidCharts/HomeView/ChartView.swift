7d86d902ba0851ca117a6eb4b1bd71841e97f6ca
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-04-16 13:33:24 +0200

    moved chartview to homeview folder

--- CovidCharts/HomeView/ChartView.swift
+++ CovidCharts/HomeView/ChartView.swift
@@ -8,6 +8,12 @@
 
 import SwiftUI
 
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView(vm: ChartViewModel())
+    }
+}
+
 struct ChartView: View {
     
     @ObservedObject var vm: ChartViewModel
@@ -15,11 +21,14 @@ struct ChartView: View {
     var body: some View {
         VStack (alignment: .center, spacing: 8) {
             ChartTopView(vm: vm)
+            
             ChartContentView(vm: vm)
             ChartBottomView(vm: vm)
         }
         .frame(width: UIScreen.screenWidth-16, height: UIScreen.screenHeight/1.75)
-        .background(Color(UIColor.clear))
+        .background(Color(UIColor.systemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .shadow(color: Color.black.opacity(0.2), radius: 6, x: 0, y: 6)
     }
     
 }
