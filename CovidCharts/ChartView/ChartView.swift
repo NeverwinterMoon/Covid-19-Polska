66e8eb59740c65617fe0305377edac5d7b3c5ae6
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-04-21 09:20:46 +0200

    added bacckground color to chartvivew for lists

--- CovidCharts/ChartView/ChartView.swift
+++ CovidCharts/ChartView/ChartView.swift
@@ -38,6 +38,7 @@ struct ChartView: View {
         .background(Colors.customViewBackground)
         .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
         .shadow(color: Color.black.opacity(0.2), radius: 8, x: 0, y: 5)
+        .listRowBackground(Colors.appBackground)
     }
     
 }
