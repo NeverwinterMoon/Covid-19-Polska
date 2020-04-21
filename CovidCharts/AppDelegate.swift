a4ecf9223e8d9861eee80c89f7d426c804a8acc0
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-04-21 09:20:28 +0200

    default view of list

--- CovidCharts/AppDelegate.swift
+++ CovidCharts/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITableView.appearance().backgroundColor = UIColor.clear
+        UITableView.appearance().separatorStyle = .none
         // Override point for customization after application launch.
         return true
     }
