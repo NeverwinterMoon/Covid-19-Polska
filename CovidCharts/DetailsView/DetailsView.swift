f5b26ab64c80d30e6ed14cc87807d6dc29b29b0b
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-04-21 09:21:34 +0200

    detailsView update

--- CovidCharts/DetailsView/DetailsView.swift
+++ CovidCharts/DetailsView/DetailsView.swift
@@ -13,23 +13,32 @@ struct DetailsView: View {
     @EnvironmentObject var vm: ChartViewModel
     @Binding var showDetailsView: Bool
     
+    @State var showConfirmedCases: Bool = false
+    
     var body: some View {
         ZStack {
             Colors.appBackground
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 VerticalSpacer()
-                TitleView(title: "Szczegóły", lastUpdateTime: vm.getLastUpdateDate(), parameterSumValue: vm.getConfirmedCases(), parameterIcon: Images.confirmed, parameterIncreaseValue: vm.getLatestIncrease(), rightButtonIcon: Images.reload) {
+                TitleView(title: "Covid-19 Polska", lastUpdateTime: vm.getLastUpdateDate(), parameterSumValue: vm.getConfirmedCases(), parameterIcon: Images.confirmed, parameterIncreaseValue: vm.getLatestIncrease(), rightButtonIcon: Images.reload) {
                     self.showDetailsView.toggle()
                 }
                 VerticalSpacer()
                 VerticalSpacer()
                 List {
-                    Text("Dupa")
-                        .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
-                    Spacer()
-                }
-                .background(Colors.customViewBackground)
+                    ExpandableLineView(title: "Zakażenia", show: $showConfirmedCases)
+                    ChartView(chartData: vm.getDailyChangeData(), title: "Dzienny przyrost", todayValue: vm.getTodayValue(), maxY: vm.getChartMaxValue(), midY: vm.getChartMidValue(), minX: vm.getMinDate(), maxX: vm.getMaxDate())
+                        .frame(height: showConfirmedCases ? 550 : -550)
+                        .opacity(showConfirmedCases ? 1.0 : 0.0)
+                        .animation(.spring(response: 0.5, dampingFraction: 0.80, blendDuration: 0.9), value: showConfirmedCases)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    ExpandableLineView(title: "Zgony", show: $showConfirmedCases)
+                    ExpandableLineView(title: "Wyleczeni", show: $showConfirmedCases)
+                    ExpandableLineView(title: "Województwa", show: $showConfirmedCases)
+                    ExpandableLineView(title: "Statystyki globalne", show: $showConfirmedCases)
+                }.environment(\.defaultMinListRowHeight, 1)
+                .background(Colors.appBackground)
             }
         }
 
@@ -42,12 +51,34 @@ struct DetailsView_Previews: PreviewProvider {
     }
 }
 
-
-struct DismissButtonView: View {
+struct ExpandableLineView: View {
+    
+    var title: String
+    @Binding var show: Bool
+    
     var body: some View {
-        Image(systemName: Images.dismiss)
-            .font(.system(size: 30, weight: .bold))
-            .frame(width: 40, height: 40)
-            .foregroundColor(Color(UIColor.systemGray5))
+        HStack {
+            HStack (spacing: 0) {
+                Text(title)
+                    .font(Fonts.titleListElement)
+                    .padding(.leading, 16)
+                    .padding()
+                    .animation(.linear)
+                IconView(name: Images.expand, size: .medium, weight: .semibold, color: Colors.main)
+                    .padding(.horizontal, 8)
+                    .rotationEffect(.degrees(show ? 360 : 180))
+                    .animation(.easeInOut)
+            }
+            .frame(height: 50)
+            .background(Colors.customViewBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 8, x: 0, y: 5)
+            Spacer()
+        }
+        .onTapGesture {
+                self.show.toggle()
+        }
+        .listRowBackground(Colors.appBackground)
+        .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
     }
 }
